9165935526a0aafdc76bbdb77269465521766347
malix <288672436+Mohamed-malix@users.noreply.github.com>
2026-08-31 17:47:49 +0300

    Add the CSS styling for the form

---
+++ main.js
@@ -0,0 +1,26 @@
+
+const books=[];
+
+function Book(name,author,pages,red,comments){
+
+    if(!new.target){
+        console.log("You need to use 'new' key word!");
+    }
+
+    this.name=name;
+    this.author=author;
+    this.pages=pages;
+    this.red=red;
+    this.rating=rating;
+
+}
+
+function addToLibrary(book){
+
+}
+
+
+
+
+
+
